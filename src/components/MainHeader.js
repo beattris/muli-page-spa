a4a3c75aa892cd798bf143c4b1444dfa3de55d18
Beattris <beatriceebirim@gmail.com>
2022-03-08 16:16:20 +0100

    imported NavLink to make the navigation elements show the active one

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+import classes from './MainHeader.module.css';
 
 const MainHeader = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <nav>
         <ul>
           <li>
-            <Link to="/welcome">Welcome</Link>
+            <NavLink activeClassName={classes.active} to="/welcome">Welcome</NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink activeClassName={classes.active} to="/products">Products</NavLink>
           </li>
         </ul>
       </nav>
