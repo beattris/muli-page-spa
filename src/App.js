662f838145470b25087454394af05473c3082113
Beattris <beatriceebirim@gmail.com>
2022-03-08 15:59:10 +0100

    imported Link to link to different pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Route } from "react-router-dom";
-import Products from "./components/Products";
-import Welcome from "./components/Welcome";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
