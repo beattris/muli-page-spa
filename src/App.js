69cb0bf7dd8a8cd0022b6b4ba504ef4075774fb4
Beattris <beatriceebirim@gmail.com>
2022-03-08 15:30:57 +0100

    imported Route and BrowserRouter to route to different pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route } from "react-router-dom";
+import Products from "./components/Products";
+import Welcome from "./components/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
