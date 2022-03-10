a7cf8d2e5e68cfc9c2263cc82ed098d8856a4ff1
Beattris <beatriceebirim@gmail.com>
2022-03-10 13:54:17 +0100

    added brand new components for the main app build

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import { Route, Switch, Redirect } from "react-router-dom";
-
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
-import Products from "./pages/Products";
-import Welcome from "./pages/Welcome";
-
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom';
 
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
