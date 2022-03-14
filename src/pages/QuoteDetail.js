d97e75ce451717d68157cbd623a7a6ccc36a301f
Beattris <beatriceebirim@gmail.com>
2022-03-14 03:36:40 +0100

    added a nested route to conditional show a link depending on which route the app is currently on

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
 const DUMMY_QUOTES = [
@@ -15,13 +15,20 @@ const QuoteDetail = () => {
   // finding the exact quote detail that matches the URL
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if(!quote){
-    return <p>No quote found</p>
+  if (!quote) {
+    return <p>No quote found</p>;
   }
 
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       {/* used dynamic routing as it depends on "quoteId" for which this quote detail component was loaded*/}
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
