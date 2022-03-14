a3857945bfd99768318f7b8f2ad8816d5af9915b
Beattris <beatriceebirim@gmail.com>
2022-03-14 02:35:49 +0100

    implemented programmatic navigation with react-router

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,11 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
