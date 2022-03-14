7097a97b0811346c7ad1e18c2362bb958590f106
Beattris <beatriceebirim@gmail.com>
2022-03-14 02:23:32 +0100

    added a 404 page component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
 import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
@@ -15,11 +16,14 @@ function App() {
         <Route path="/quotes" exact>
           <AllQuotes />
         </Route>
-        <Route path="/quotes/:quoteId">
+        <Route path="/quotes/:quoteId" exact>
           <QuoteDetail />
         </Route>
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path='*'>
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
