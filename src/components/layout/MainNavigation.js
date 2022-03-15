ec6c6b3976a561c4a5c815b40894d6156e0592a5
Beattris <beatriceebirim@gmail.com>
2022-03-15 06:38:56 +0100

    added images for the README.md

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
@@ -5,7 +6,7 @@ import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>Great Quotes</div>
+      <div className={classes.logo}><Link to='/quotes' style={{textDecoration:'none', color: 'white'}}>Great Quotes</Link></div>
       <nav className={classes.nav}>
         <ul>
           <li>
